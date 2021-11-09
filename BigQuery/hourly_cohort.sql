2c20ef90d6992d889ae7d7cc697d96c244d2fcb8
jchen950 <jchen950@gatech.edu>
2021-11-09 18:10:51 -0500

    update onset logic

--- BigQuery/hourly_cohort.sql
+++ BigQuery/hourly_cohort.sql
@@ -1,12 +1,11 @@
 
 -- define sepsis onset event if
 --  1. has suspected_infection_time from derived.suspinfect_poe
---  2. sofa score changed by >=2 during a time window around the suspected_infection_time
+--  2. sofa score >=2 for the first time during a time window around the suspected_infection_time
 
--- create data with icustay with sofa change >= 2
+-- create data with icustay with sofa >= 2
 create or replace table `cdcproject.BDFH.sofa_delta` as
 -- get sofa scores for the 72 time window around suspected_infection_time
--- exclude sofa score == 0 (mostly caused by missing in early horizons)
 with step1 as 
 (
     select 
@@ -19,16 +18,7 @@ with step1 as
     from `physionet-data.mimiciii_derived.pivoted_sofa` ps
     inner join `physionet-data.mimiciii_derived.suspinfect_poe` sp
     using (icustay_id)
-    where sp.suspected_infection_time is not null and datetime_diff(ps.starttime, sp.suspected_infection_time, hour) between -48 and 24 and ps.SOFA_24hours>0
-),
--- calculate sofa score change compared to min score by far
-step2 as
-(
-select
-*,
-min(sofa_24hours) over (partition by icustay_id order by hr) as min_sofa,
-sofa_24hours - min(sofa_24hours) over (partition by icustay_id order by hr) as delta_score
-from step1
+    where sp.suspected_infection_time is not null and datetime_diff(ps.starttime, sp.suspected_infection_time, hour) between -48 and 24
 ),
 -- define sepsis onset time as the endtime when delta_score>=2
 step3 as
@@ -37,11 +27,9 @@ step3 as
     icustay_id,
     suspected_infection_time,
     endtime as sepsis_onset,
-    delta_score,
-    min_sofa,
     sofa_24hours
-    from step2
-    where delta_score >= 2
+    from step1
+    where sofa_24hours >= 2
 ),
 -- keep the first record with delta_score>=2
 step4 as
@@ -62,7 +50,6 @@ select sd.icustay_id
     , s3c.outtime
     , datetime_diff(s3c.outtime, s3c.intime, second)
           / 60.0 / 60.0 as length_of_stay
-    , sd.delta_score
     , sd.sepsis_onset
     , datetime_diff(sd.sepsis_onset, s3c.intime, second)
           / 60.0 / 60.0 / 24.0 as sepsis_onset_day
@@ -77,7 +64,7 @@ on s3c.hadm_id = adm.hadm_id
 inner join `physionet-data.mimiciii_clinical.icustays` icu
 on s3c.icustay_id = icu.ICUSTAY_ID
 where not (
-                s3c.age <= 14 -- CHANGED FROM ORIGINAL! <=16
+                s3c.age <= 16
                     or adm.HAS_CHARTEVENTS_DATA = 0
                     or s3c.intime is null
                     or s3c.outtime is null
@@ -95,7 +82,6 @@ select s3c.icustay_id
     , s3c.outtime
     , datetime_diff(s3c.outtime, s3c.intime, second)
           / 60.0 / 60.0 as length_of_stay
-    , sd.delta_score
     , sd.sepsis_onset
     , icu.SUBJECT_ID
     from `cdcproject.BDFH.Cohort` s3c
@@ -113,7 +99,7 @@ select s3c.icustay_id
             from `physionet-data.mimiciii_clinical.diagnoses_icd` dg 
             where dg.icd9_code in ('78552','99591','99592'))
         and not (
-            s3c.age <= 14 -- CHANGED FROM ORIGINAL! <=16
+            s3c.age <= 16
             or adm.HAS_CHARTEVENTS_DATA = 0
             or s3c.intime is null
             or s3c.outtime is null
